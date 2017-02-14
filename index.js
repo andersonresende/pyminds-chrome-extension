f34e181c72ae128a2edb306a206939e8bcf575be
Anderson Resende <andersonresende86@gmail.com>
2017-02-13 23:24:29 -0300

    Updated status message

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var statusDisplay = null;
+
 function addQuestion() {
     event.preventDefault();
 
@@ -16,30 +18,23 @@ function addQuestion() {
 
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-    // Handle request state change events
-    // xhr.onreadystatechange = function() { 
-    //     // If the request completed
-    //     if (xhr.readyState == 4) {
-    //         statusDisplay.innerHTML = '';
-    //         if (xhr.status == 200) {
-    //             // If it was a success, close the popup after a short delay
-    //             statusDisplay.innerHTML = 'Saved!';
-    //             window.setTimeout(window.close, 1000);
-    //         } else {
-    //             // Show what went wrong
-    //             statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
-    //         }
-    //     }
-    // };
+    xhr.onreadystatechange = function() { 
+        if (xhr.readyState == 4) {
+            statusDisplay.innerHTML = '';
+            if (xhr.status == 201) {
+                statusDisplay.innerHTML = 'Created question!';
+                window.setTimeout(window.close, 2000);
+            } else {
+                statusDisplay.innerHTML = 'Error creating: ' + xhr.statusText;
+            }
+        }
+    };
 
     xhr.send(params);
-    // statusDisplay.innerHTML = 'Saving...';
+    statusDisplay.innerHTML = 'Posting question...';
 }
 
-// When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
-    // Cache a reference to the status display SPAN
-    // statusDisplay = document.getElementById('status-display');
-    // Handle the bookmark form submit event with our addBookmark function
+    statusDisplay = document.getElementById('status-display');
     document.getElementById('addQuestion').addEventListener('submit', addQuestion);
 });
